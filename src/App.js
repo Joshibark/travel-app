e60a9a9741fd57148ac0736dead0be52095b82d9
Joshibark <josiah.barkstrom@gmail.com>
2022-05-03 11:42:58 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,37 @@
+import './main.css';
+
+function Main() {
+    return (
+      <div className="Main">
+          <h1>Travel companion</h1>
+
+          <div className="map">
+            <h1>map</h1>
+          </div>
+
+            <div className='search-bar'>
+                <input className='search' type="text" placeholder='Enter position...'/>
+                <input className='search' type="text" placeholder='Enter destination...'/>
+            </div>
+
+          <div className='travel-alternative'>
+            <p>travel alternative dropdown</p>
+            {/* dropdown eller flera alternativ som i jlt appen??? */}
+          </div>
+
+          <div className='text'>
+              {/* Dropdown details */}
+            <h2>Travel alternative: Gå</h2>
+            <h2>Distans: 0km</h2>
+            <h2>Emission: 0 CO<sub>2</sub></h2>
+          </div>
+
+          <div className='Language'>
+              <h1>Swedish/English</h1>
+              {/* I settings istället??? */}
+          </div>
+      </div>
+    );
+  }
+  
+  export default Main;
